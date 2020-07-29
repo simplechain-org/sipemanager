7fe2fb46dac34500fff6cc4fdcda48a13f53a5d2
teachain <281477138@qq.com>
2020-07-29 15:31:10 +0800

    fix chain register bug

--- resource/database.sql
+++ resource/database.sql
@@ -1,2 +1,2 @@
-drop database if exists `sipe`;
-create database `sipe` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+drop database if exists `sipe_manager`;
+create database `sipe_manager` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
