a645f7a48368db0c6c65db39d7994841c02a8386
teachain <281477138@qq.com>
2020-07-31 16:24:32 +0800

    error code

--- controllers/block.go
+++ controllers/block.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"math/big"
-	"net/http"
 	"sipemanager/utils"
 	"strconv"
 	"strings"
@@ -11,7 +10,6 @@ import (
 	"sipemanager/blockchain"
 	"sipemanager/dao"
 
-	"github.com/gin-gonic/gin"
 	"github.com/simplechain-org/go-simplechain/accounts/abi"
 	"github.com/simplechain-org/go-simplechain/common"
 	"github.com/simplechain-org/go-simplechain/core/types"
@@ -23,36 +21,6 @@ type Controller struct {
 	dao         *dao.DataBaseAccessObject
 	NodeChannel chan BlockChannel
 	group       sync.WaitGroup
-}
-
-//获取一个连接到节点的连接
-func (this *Controller) getBlockChainApi(userId uint) (*blockchain.Api, error) {
-	api, ok := this.userClient[userId]
-	if !ok {
-		node, err := this.dao.GetUserCurrentNode(userId)
-		if err != nil {
-			return nil, err
-		}
-		chain, err := this.dao.GetChain(node.ChainId)
-		if err != nil {
-			return nil, err
-		}
-		n := &blockchain.Node{
-			Address:   node.Address,
-			Port:      node.Port,
-			ChainId:   node.ChainId,
-			IsHttps:   node.IsHttps,
-			NetworkId: chain.NetworkId,
-		}
-
-		api, err := blockchain.NewApi(n)
-		if err != nil {
-			return nil, err
-		}
-		this.userClient[userId] = api
-		return api, nil
-	}
-	return api, nil
 }
 
 type Block struct {
@@ -67,118 +35,6 @@ type Block struct {
 	Nonce        uint64         `json:"nonce"`
 	Transactions int            `json:"transactions"`
 }
-
-//分页获取区块列表
-// @Summary 分页获取区块列表
-// @Tags block
-// @Accept  json
-// @Produce  json
-// @Security ApiKeyAuth
-// @Success 200 {object} JsonResult{data=[]Block}
-// @Router /block/list [get]
-func (this *Controller) GetPageBlock(c *gin.Context) {
-	user, err := this.GetUser(c)
-	if err != nil {
-		this.echoError(c, err)
-		return
-	}
-	api, err := this.getBlockChainApi(user.ID)
-	if err != nil {
-		this.echoError(c, err)
-		return
-	}
-	//获取最新区块
-	block, err := api.BlockByNumber(nil)
-	if err != nil {
-		this.echoError(c, err)
-		return
-	}
-	var pageSize uint64 = 10
-	//总记录数,区块号是从0开始计算的，所以这里要加1
-	var total uint64 = block.Number().Uint64() + 1
-	//总页数
-	var totalPage = total / pageSize
-	//如果不能除尽，那么就需要加1
-	if total%pageSize != 0 {
-		totalPage++
-	}
-	//当前页（默认为第一页）
-	var currentPage uint64 = 1
-	currentPageStr := c.Query("currentPage")
-	if currentPageStr != "" {
-		page, err := strconv.ParseUint(currentPageStr, 10, 64)
-		if err == nil {
-			currentPage = page
-		}
-	}
-	var start uint64 = 0
-	if total >= (currentPage-1)*pageSize+1 {
-		start = total - (currentPage-1)*pageSize - 1
-	}
-	var end uint64 = 0
-	if start >= pageSize {
-		end = start - pageSize + 1
-	} else {
-		end = 0
-	}
-	blocks := make([]Block, 0)
-	for i := start; i >= end; i-- {
-		block, err := api.BlockByNumber(big.NewInt(0).SetUint64(i))
-		if err != nil {
-			continue
-		}
-		blocks = append(blocks, Block{
-			ParentHash:   block.ParentHash(),
-			UncleHash:    block.UncleHash(),
-			CoinBase:     block.Coinbase(),
-			Difficulty:   block.Difficulty(),
-			Number:       block.Number(),
-			GasLimit:     block.GasLimit(),
-			GasUsed:      block.GasUsed(),
-			Time:         block.Time(),
-			Nonce:        block.Nonce(),
-			Transactions: len(block.Transactions()),
-		})
-		if i == 0 {
-			break
-		}
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"result": map[string]interface{}{"data": blocks, "total": total},
-		"code":   0,
-	})
-}
-
-//目前采取的策略是:用户切换节点时，把原先已有的连接关闭，根据新的node信息重新建立一个连接
-//如果没有切换节点就一直保持用已有的连接
-func (this *Controller) onChangeNode(userId uint) (*blockchain.Api, error) {
-	if _, ok := this.userClient[userId]; ok {
-		this.userClient[userId].Close()
-		delete(this.userClient, userId)
-	}
-	node, err := this.dao.GetUserCurrentNode(userId)
-	if err != nil {
-		return nil, err
-	}
-	chain, err := this.dao.GetChain(node.ChainId)
-	if err != nil {
-		return nil, err
-	}
-	n := &blockchain.Node{
-		Address:   node.Address,
-		Port:      node.Port,
-		ChainId:   node.ChainId,
-		IsHttps:   node.IsHttps,
-		NetworkId: chain.NetworkId,
-	}
-	api, err := blockchain.NewApi(n)
-	this.userClient[userId] = api
-	if err != nil {
-		return nil, err
-	}
-	return api, nil
-}
-
 func (this *Controller) SyncBlock(api *blockchain.Api, number int64, node dao.InstanceNodes) {
 	chainId := node.ChainId
 	//fmt.Printf("----当前写入区块号:%+v, ----当前ChainId： %+v ————\n", number, chainId)
